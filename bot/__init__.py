75d762d6cf90976f0f1f2c250312810a70690232
Nelson Estevão <nelsonmestevao@gmail.com>
2020-10-27 16:58:59 +0000

    Improve setup process (#2)

---
+++ bot/__init__.py
@@ -0,0 +1,5 @@
+"""Gongo
+A bot for the CoderDojo Braga Discord server. Its goal is to help the Discord
+admins promote members in a more festive and easy way.
+"""
+VERSION = '0.0.1'
---
+++ bot/__main__.py
@@ -0,0 +1,5 @@
+from bot.client import Gongo
+from bot.settings import BOT_TOKEN
+
+if __name__ == '__main__':
+    Gongo().run(BOT_TOKEN)
