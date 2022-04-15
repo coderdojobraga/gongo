aa89b22386dde9ef496fcaec73a9069a2f5554f8
daniel_sp <danielsp356@gmail.com>
2022-04-15 23:59:16 +0100

    Belt attribuitons complete

--- bot/client.py
+++ bot/client.py
@@ -7,7 +7,7 @@ import traceback
 from bot import VERSION
 
 client = commands.Bot(
-    command_prefix = commands.when_mentioned_or('!g'),
+    command_prefix = commands.when_mentioned_or('$'),
     help_command = None,
     intents = discord.Intents.all(),
     case_insensitive = True
@@ -22,7 +22,7 @@ cinturoes = ["Branco",
              "Roxo", 
              "Preto"]
 
-def get_belt_from_name(guild, belt_name):
+def get_role_from_name(guild, belt_name):
     for role in guild.roles:
         if role.name == belt_name:
             return role
@@ -43,7 +43,7 @@ class Ninja_data():
         for belt in self.member.roles:
             for i in range(len(cinturoes)-1):
                 if cinturoes[i] == belt.name:
-                    return get_belt_from_name(self.guild, cinturoes[i+1])
+                    return get_role_from_name(self.guild, cinturoes[i+1])
     
 
 @client.event
@@ -51,8 +51,8 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 
-@client.command(alias=" promove")
-@commands.has_role("🛡️ Admin")
+@client.command()
+@commands.has_any_role("🛡️ Admin", "🏆 Champion", "🧑‍🏫 Mentores")
 async def promove(ctx, user, belt):
     await ctx.send(
         f"<@{ctx.message.author.id}> quer promover {user} a {belt}"
@@ -63,8 +63,16 @@ async def promove(ctx, user, belt):
     member = guild.get_member(mentions[0]) #gets the member of the mentioned id           
 
     ninja = Ninja_data(guild, member)
-
-    if ninja.next_belt().name == belt:  
+    
+    if belt == ninja.current_belt().name:
+        await ctx.send(
+            f'<@{ctx.message.author.id}> esse ja é o cinturão atual do ninja {user}'
+            )
+    elif ninja.next_belt().name == belt:  
+        
+        await member.add_roles(guild.get_role(ninja.next_belt().id), reason=None, atomic=True)
+        await member.remove_roles(guild.get_role(ninja.current_belt().id), reason=None, atomic=True)
+        
         await ctx.send(
             f'{user} agora és cinturão {belt}'
             )
