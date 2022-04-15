42437cfd218d0235250d47c3acffb5be9ea9699d
daniel_sp <danielsp356@gmail.com>
2022-04-15 13:59:42 +0100

    Restruturação dos comandos

--- bot/client.py
+++ bot/client.py
@@ -1,42 +1,85 @@
-"""Gongo.
-
-Usage:
-  Gongo (promove | p) <user> a <belt>
-
-"""
+from tabnanny import check
+from unicodedata import name
 import discord
-from docopt import docopt, DocoptExit
+from discord.ext import commands
+import traceback
 
 from bot import VERSION
 
+client = commands.Bot(
+    command_prefix = commands.when_mentioned_or('!g'),
+    help_command = None,
+    intents = discord.Intents.all(),
+    case_insensitive = True
+)
 
-class Gongo(discord.Client):
-    async def on_ready(self):
-        print(f'We have logged in as {self.user}!')
+cinturoes = ["Branco", 
+             "Amarelo", 
+             "Laranja", 
+             "Verde", 
+             "Azul", 
+             "Vermelho", 
+             "Roxo", 
+             "Preto"]
 
-    async def on_message(self, message):
-        if message.author.bot:
-            return
+def get_belt_from_name(guild, belt_name):
+    for role in guild.roles:
+        if role.name == belt_name:
+            return role
 
-        if not message.content.lower().startswith('gongo'):
-            return
+class Ninja_data(): 
 
-        if not "🛡️ Admin" in [role.name for role in message.author.roles]:
-            await message.channel.send(
-                "Não tens permissões para me usar. Tenta com sudo :clown:"
+    def __init__(self, guild, member):
+        self.guild = guild
+        self.member = member 
+
+    def current_belt(self):
+        for belt in self.member.roles:
+            for i in range(len(cinturoes)-1):
+                if cinturoes[i] == belt.name:
+                    return belt
+    
+    def next_belt(self):
+        for belt in self.member.roles:
+            for i in range(len(cinturoes)-1):
+                if cinturoes[i] == belt.name:
+                    return get_belt_from_name(self.guild, cinturoes[i+1])
+    
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.command(alias=" promove")
+@commands.has_role("🛡️ Admin")
+async def promove(ctx, user, belt):
+    await ctx.send(
+        f"<@{ctx.message.author.id}> quer promover {user} a {belt}"
+    ) 
+
+    mentions = ctx.message.raw_mentions    #List of id mentions in the command
+    guild = ctx.guild                      #Guild of the command
+    member = guild.get_member(mentions[0]) #gets the member of the mentioned id           
+
+    ninja = Ninja_data(guild, member)
+
+    if ninja.next_belt().name == belt:  
+        await ctx.send(
+            f'{user} agora és cinturão {belt}'
             )
-            return
+    else: 
+        await ctx.send(
+            f'<@{ctx.message.author.id}> esse cinturão não é valido de se ser atribuido'
+            )
 
-        try:
-            arguments = docopt(__doc__,
-                               argv=message.content.split()[1:],
-                               help=False)
 
-            if arguments['promove'] or arguments['p']:
-                await message.channel.send(
-                    f'<@{message.author.id}> quer promover {arguments["<user>"]} a cinturão {arguments["<belt>"]} :tada:'
-                )
+@client.event
+async def on_command_error(ctx, error: traceback.format_exc()):
+    if isinstance(error, commands.UserInputError):
+        embed = discord.Embed(
+            title = 'ERRO',
+            description = f'Olá! Esse comando não se usa assim! Vê isto: ...',
+            color = discord.Color(0xcc7)
+        )
 
-        except DocoptExit:
-            await message.channel.send(
-                f'Olá! Esse comando não é válido! Vê isto: ```{__doc__}```')
